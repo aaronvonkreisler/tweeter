1df1466529eb2d829dbe695b0788197ad6da0707
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-11-25 14:47:03 -0600

    test

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true,
+   },
+   userName: {
+      type: String,
+      required: true,
+      unique: true,
+   },
+   displayName: {
+      type: String,
+   },
+   email: {
+      type: String,
+      required: true,
+      unique: true,
+   },
+   password: {
+      type: String,
+      required: true,
+   },
+   dateOfBirth: {
+      type: Date,
+      required: true,
+   },
+   createdAt: {
+      type: Date,
+      default: Date.now,
+   },
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
