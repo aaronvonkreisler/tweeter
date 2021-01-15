ef976ccf480041db790b42c68c32b3f68d2b8bea
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2021-01-14 18:20:39 -0600

    added route handler for messages

--- dist/Resources/messages/message/messages.router.js
+++ dist/Resources/messages/message/messages.router.js
@@ -7,6 +7,11 @@ exports.default = void 0;
 
 var _express = require("express");
 
-var router = (0, _express.Router)();
+var _messages = require("./messages.controller");
+
+var router = (0, _express.Router)(); // @route            POST api/messages/
+// @description      Send a new Message
+
+router.post('/', _messages.sendMessage);
 var _default = router;
 exports.default = _default;
--- src/Resources/messages/message/messages.router.js
+++ src/Resources/messages/message/messages.router.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import { sendMessage } from './messages.controller';
 
 const router = Router();
+
+// @route            POST api/messages/
+// @description      Send a new Message
+router.post('/', sendMessage);
 
 export default router;
